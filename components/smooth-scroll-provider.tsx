2d5ac3a0abf39ff08afa282e8db4296bdf5cfe7e
JackMatthew112233 <rejekkimanalu22@gmail.com>
2025-10-25 22:30:32 +0800

    Updates

--- components/smooth-scroll-provider.tsx
+++ components/smooth-scroll-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { ReactNode, useEffect } from 'react';
-import Lenis from 'lenis';
+import { ReactNode, useEffect } from "react";
+import Lenis from "lenis";
 
 interface SmoothScrollProviderProps {
   children: ReactNode;
@@ -12,13 +12,6 @@ const SmoothScrollProvider = ({ children }: SmoothScrollProviderProps) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
     });
 
     function raf(time: number) {
